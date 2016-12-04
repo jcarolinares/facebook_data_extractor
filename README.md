c49dd1b37b8cee453410729c7c71b0d13b9b18ea
jcarolinares <jcarolinares@gmail.com>
2016-12-04 13:23:59 +0100

    Initial commit, timeline extactor done

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # facebook_data_extractor
 A Python parser that extracts the info you want from tan official downloaded facebook archive
+
+Made by Julián Caro Linares
+
+* email: jcarolinares@gmail.com
+* twitter: @jcarolinares
+
+#How to use
+Python version and libraries:
+* Python 3
+* BeautifulSoup
+
+First of all, if you don't have Python 3.X on your computer, you'll need to install it.
+
+<a href="https://www.python.org/downloads/">Official Python 3 page</a>
+
+You'll also need the library BeautifulSoup:
+
+$ apt-get install python-bs4
+
+Or
+
+$ pip install beautifulsoup4
+
+<a href="https://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-beautiful-soup">More info</a>
+
+After that, you'll need to download your Facebook data.
+
+Follow the <a href="https://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-beautiful-soup"> official Facebook guide</a>, section "Download info":
+
+Download Your Info: This includes a lot of the same information available to you in your account and activity log, including your Timeline info, posts you have shared, messages, photos and more. Additionally, it includes information that is not available simply by logging into your account, like the ads you have clicked on, data like the IP addresses that are logged when you log into or out of Facebook, and more. To download your information, go to your Settings and click Download a copy of your Facebook data.
+
+
+You'll probably have to wait an email from Faccebook with the data link.
+
+When you had downloaded your facebook data, extract the file in the same folder of this program.
+
+Now executes the python program "facebook_data_extractor.py" using python3
+
+* Your facebook name ID is the name that comes in your facebook data folder:
+  In the case "facebook-yournamessurnames" you should write "yournamessurnames"
+
+* Write the keywords for your search separated with commas, for example, "train,happy,dog"
+
+If the program founds comments that match these keywords it'll show them in the terminal.
+
+You also have a new file called "timeline_results-yourkeywords.txt" where you have all the search results with their date.
+
+
+
+=============
+
+<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Licencia Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Facebook data extractor</span> por <span xmlns:cc="http://creativecommons.org/ns#" property="cc:attributionName">Julián Caro Linares</span> licensed by <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.<br /><br />
---
+++ facebook_data_extractor.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+Facebook data extractor
+
+A Python parser that extracts the info that you want from an official downloaded facebook archive
+
+Made by Julián Caro Linares
+
+email: jcarolinares@gmail.com
+twitter: @jcarolinares
+
+License: CC-BY-SA
+
+'''
+
+#Libraries
+from bs4 import BeautifulSoup
+import requests
+
+#Global variables
+user=""
+search_keywords=""
+info_type={"ads":"ads.htm","contacs":"contact_info.htm","events":"events.htm","friends":"friends.htm","messages":"messages.htm","mobile_devices":"mobile_devices.htm","photos":"photos.htm","places":"places.htm","pokes":"pokes.htm","security":"security.htm","surveys":"survey_responses.htm","synced_photos":"synced_photos.htm","timeline":"timeline.htm","videos":"videos.htm"}
+
+#Functions
+class comment:
+    def new(self,user,date,comment,keywords):
+        self.user=str(user)
+        self.date=str(date)
+        self.comment=str(comment)
+        self.keywords=str(keywords)
+
+    def print_comment():
+        print("\nDate: \n")
+        print(self.date)
+        print("Comment: \n")
+        print(self.comment)
+
+    def get_user(self):
+        return self.user
+    def get_date(self):
+        return self.date
+    def get_comment(self):
+        return self.comment
+    def get_keywords(self):
+        return self.keywords
+
+def timeline_parser():
+    global user
+    search_keywords=input("\n\nInsert your search keywords separated with commas:\n")
+    search_keywords=search_keywords.lower().split(",")
+
+    file_route='./facebook-'+user+'/html/'+info_type["timeline"]
+
+    #Data parser
+    #Extracting the file
+    soup=BeautifulSoup(open(file_route),'lxml')
+
+    comments_text=soup.find_all('div',class_="comment") #It's only takes real comments, not share or likes. To take this raw info, use the dates_text instead
+    dates_text=soup.find_all('div',class_="meta") #Used for taking addional info like likes, shared links or others. No formated info
+
+
+    #Making the file name
+    file_name="timeline_results"
+    for keyword in search_keywords:
+        file_name=file_name+"-"+keyword
+
+    file_name=file_name+".txt"
+    f=open(file_name,"a")
+
+    #Looking for the desired comments
+    for index in range(len(comments_text)):
+
+        for keyword in search_keywords:
+            if(keyword in comments_text[index].get_text().lower()):
+                print("\n\n"+comments_text[index].find_previous_sibling().get_text())#Takes the date of the comment
+                print (comments_text[index].get_text())
+                f.write("\n\n"+comments_text[index].find_previous_sibling().get_text())
+                f.write("\n\n"+comments_text[index].get_text()+"\n\n")
+                break
+
+
+    #Used for taking addional info like "likes", shared links and others. Not formated info, putted at the end of the file results
+    print("\n\n---ADITIONAL INFO, SHARES AND LIKES---\n")
+    f.write("\n\n---ADITIONAL INFO, SHARES AND LIKES---\n")
+    for date in dates_text:
+        for keyword in search_keywords:
+            if(keyword in date.next_element.next_element):
+                print(date.get_text())
+                print(date.next_element.next_element)
+                f.write("\n\n"+date.get_text())
+                f.write("\n\n"+date.next_element.next_element+"\n\n")
+                break
+
+    f.close()
+
+    #Facebook timeline page syntax
+    #print("\nYou published "+str(len(comments_text))+" comments in your timeline")
+
+    #"<div class="meta">Friday, 2 December 2016 at 15:33 UTC+01</div>"
+    #<div class="comment">This is a comment</div>
+
+#Main execution
+def main():
+    global user
+    user=input("\n\nInsert your facebook name ID (namesurnames):\n")
+
+    timeline_parser()#searchs and extracts data from the timeline.htm data
+
+
+if __name__ == "__main__":
+ main()
