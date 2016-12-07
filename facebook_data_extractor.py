d3cd5b0151fbfe765997f30fd53224f47a57b768
jcarolinares <jcarolinares@gmail.com>
2016-12-07 02:06:17 +0100

    Contacts extractor in progresss

--- facebook_data_extractor.py
+++ facebook_data_extractor.py
@@ -111,13 +111,108 @@ def timeline_parser():
     #"<div class="meta">Friday, 2 December 2016 at 15:33 UTC+01</div>"
     #<div class="comment">This is a comment</div>
 
+
+def contacts_parser():
+    global user
+    contacts_counter=0
+    #search_keywords=input("\n\nInsert your search keywords separated with commas:\n")
+    #search_keywords=search_keywords.lower().split(",")
+
+    file_route='./facebook-'+user+'/html/'+info_type["contacs"]
+
+    #Data parser
+    #Extracting the file
+    soup=BeautifulSoup(open(file_route),'lxml')
+
+    contacts_block=soup.find_all('tr') #It's only takes real comments, not share or likes. To take this raw info, use the dates_text instead
+    #dates_text=soup.find_all('div',class_="meta") #Used for taking addional info like likes, shared links or others. No formated info
+
+    for contact in contacts_block:
+        if(contact.td):
+            print(contact.td.get_text())
+
+            if(contact.td.next_sibling):
+                contacts_number=(contact.td.next_sibling.span.find_all('li'))
+                for contact_n in contacts_number:
+                    #print(contact_n.get_text())
+                    cadena=""
+                    if ("Contact" in contact_n.get_text()):
+                        cadena=contact_n.get_text()
+                        cadena=cadena.replace("Contact","")
+                        cadena=cadena.strip()#Deletes first and last spaces
+                    print(cadena)
+        print("\n\n")
+
+    '''
+    <tr>
+        <td>Name surname surname</td>
+        <td>
+            <span class="meta">
+                <ul>
+                    <li>Contact&nbsp;+34XXXXXXXXX</li>
+                    <li>Contact&nbsp;+34XXXXXXXXX</li>
+                </ul>
+            </span>
+        </td>
+    </tr>
+    '''
+    '''
+    #Making the file name
+    file_name="timeline_results"
+    for keyword in search_keywords:
+        file_name=file_name+"-"+keyword
+
+    file_name=file_name+".txt"
+    f=open(file_name,"a")
+
+    #Looking for the desired comments
+    for index in range(len(comments_text)):
+
+        for keyword in search_keywords:
+            if(keyword in comments_text[index].get_text().lower()):
+                print("\n\n"+comments_text[index].find_previous_sibling().get_text())#Takes the date of the comment
+                print (comments_text[index].get_text())
+                f.write("\n\n"+comments_text[index].find_previous_sibling().get_text())
+                f.write("\n\n"+comments_text[index].get_text()+"\n\n")
+                comments_counter=comments_counter+1
+                break
+
+
+    #Used for taking addional info like "likes", shared links and others. Not formated info, putted at the end of the file results
+    print("\n\n--- ADDITIONAL INFO, SHARES AND LIKES ---\n")
+    f.write("\n\n--- ADDITIONAL INFO, SHARES AND LIKES ---\n")
+    for date in dates_text:
+        for keyword in search_keywords:
+            if(keyword in date.next_element.next_element):
+                print("\n"+date.get_text())
+                print("\n"+date.next_element.next_element)
+                f.write("\n\n"+date.get_text())
+                f.write("\n\n"+date.next_element.next_element+"\n\n")
+                comments_counter=comments_counter+1
+                break
+
+    f.close()
+
+    #End of the program
+    if(comments_counter>0):
+        print("\n\n---COMMENTS FOUND: ", comments_counter," ---\n")
+    else:
+        print("\n\n---NO COMMENTS FOUND---")
+
+    #Facebook timeline page syntax
+    #print("\nYou published "+str(len(comments_text))+" comments in your timeline")
+
+    #"<div class="meta">Friday, 2 December 2016 at 15:33 UTC+01</div>"
+    #<div class="comment">This is a comment</div>
+    '''
+
 #Main execution
 def main():
     global user
     user=input("\n\nInsert your facebook name ID (namesurnames):\n")
 
-    timeline_parser()#searchs and extracts data from the timeline.htm data
-
+    #timeline_parser()#searchs and extracts data from the timeline.htm data
+    contacts_parser()
 
 if __name__ == "__main__":
  main()
